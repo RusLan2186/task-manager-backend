9e5d838fb0ece692e39c3cd9efe2f53c6ea79409
Ruslan <ruslanchikbest@gmail.com>
2026-05-25 15:09:28 +0300

    Protect users endpoint with ADMIN authorization

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,9 @@
 import express from "express";
-import { authenticate } from "../middleware/auth.middleware";
+import { authenticate, authorize } from "../middleware/auth.middleware";
 import { getAllUsers } from "../controllers/user.controller";
-
 
 const router = express.Router({ mergeParams: true });
 
-router.get("/", authenticate, getAllUsers);
-
+router.get("/", authenticate, authorize("ADMIN"), getAllUsers);
 
 export { router };
